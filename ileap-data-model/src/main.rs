3128702217481a33452accd2179a429192f7413d
Raimundo Henriques <raimundo.henriques@gmail.com>
2024-11-25 14:33:10 +0100

    refactor: write_schemas (w/ @zeitgeist)

--- ileap-data-model/src/main.rs
+++ ileap-data-model/src/main.rs
@@ -8,13 +8,17 @@
 use ileap_data_model::{Hoc, ShipmentFootprint, Tad, Toc};
 use std::io::Error;
 
-use ileap_data_model::schema_gen::generate_schemas;
+use ileap_data_model::schema_gen::write_schemas;
 
 fn main() -> Result<(), Error> {
-    generate_schemas::<ShipmentFootprint>()?;
-    generate_schemas::<Toc>()?;
-    generate_schemas::<Tad>()?;
-    generate_schemas::<Hoc>()?;
+    write_schemas::<ShipmentFootprint>(
+        "ShipmentFootprint",
+        "shipment-footprint",
+        "pcf-shipment-footprint",
+    )?;
+    write_schemas::<Toc>("Toc", "toc", "pcf-toc")?;
+    write_schemas::<Tad>("Tad", "tad", "pcf-tad")?;
+    write_schemas::<Hoc>("Hoc", "hoc", "pcf-hoc")?;
 
     Ok(())
 }
--- ileap-data-model/src/schema_gen.rs
+++ ileap-data-model/src/schema_gen.rs
@@ -6,7 +6,6 @@
  */
 
 use pact_data_model::ProductFootprint;
-use regex::Regex;
 use schemars::gen::{SchemaGenerator, SchemaSettings};
 use schemars::schema::{ObjectValidation, RootSchema, Schema, SchemaObject};
 use schemars::{schema_for, Map, Set};
@@ -14,30 +13,23 @@ use serde_json::to_string_pretty;
 use std::fs::File;
 use std::io::{Error, Write};
 
-pub fn generate_schemas<T: schemars::JsonSchema>() -> Result<(), Error> {
-    let type_name = get_type_name::<T>()?;
-
-    let regex = Regex::new(r"([^A-Z])([A-Z])").unwrap();
-
-    let schema_name = regex
-        .replace_all(&type_name, "$1-$2")
-        .into_owned()
-        .to_lowercase();
-
+pub fn write_schemas<T: schemars::JsonSchema>(
+    type_name: &str,
+    schema_file_name: &str,
+    pcf_schema_file_name: &str,
+) -> Result<(), Error> {
     let schema = schema_for!(T);
 
-    write_schema_file(schema, &schema_name)?;
+    write_schema_file(schema, schema_file_name)?;
 
-    let pcf_schema = gen_pcf_with_extension::<T>()?;
+    let pcf_schema = gen_pcf_with_extension::<T>(&type_name);
 
-    write_schema_file(pcf_schema, &format!("pcf-{schema_name}"))?;
+    write_schema_file(pcf_schema, pcf_schema_file_name)?;
 
     Ok(())
 }
 
-pub fn gen_pcf_with_extension<T: schemars::JsonSchema>() -> Result<RootSchema, Error> {
-    let type_name = get_type_name::<T>()?;
-
+pub fn gen_pcf_with_extension<T: schemars::JsonSchema>(type_name: &str) -> RootSchema {
     let settings = SchemaSettings::default();
 
     let mut schema_generator = SchemaGenerator::new(settings);
@@ -106,15 +98,7 @@ pub fn gen_pcf_with_extension<T: schemars::JsonSchema>() -> Result<RootSchema, E
         metadata.description = Some(format!("Data Type \"ProductFootprint\" of PACT Tech Spec Version 2 with {} as a DataModelExtension", type_name));
     };
 
-    Ok(pcf_schema)
-}
-
-fn get_type_name<T: schemars::JsonSchema>() -> Result<String, Error> {
-    let type_name = std::any::type_name::<T>();
-    let type_name = type_name.split("::").collect::<Vec<&str>>();
-    let type_name = type_name.last().unwrap_or(&"Could not parse type name");
-
-    Ok(type_name.to_string())
+    pcf_schema
 }
 
 fn write_schema_file(schema: RootSchema, schema_name: &str) -> Result<(), Error> {
@@ -128,4 +112,68 @@ fn write_schema_file(schema: RootSchema, schema_name: &str) -> Result<(), Error>
     println!("{schema_name}.json successfully created");
 
     Ok(())
+}
+
+#[test]
+fn compare_schemas() {
+    use crate::{Hoc, ShipmentFootprint, Tad, Toc};
+    use schemars::schema_for;
+    use serde_json::to_string_pretty;
+    use serde_json::Value;
+    use std::{fs::File, io::Read};
+
+    fn read_schema(schema_name: &str) -> String {
+        let schema_dir = std::path::Path::new("schemas");
+        let mut file = File::open(schema_dir.join(schema_name)).unwrap();
+
+        let mut schema = String::new();
+        file.read_to_string(&mut schema).unwrap();
+
+        schema
+    }
+
+    fn normalize_json(json_str: &str) -> Value {
+        serde_json::from_str(json_str).unwrap()
+    }
+
+    assert_eq!(
+        normalize_json(&read_schema("shipment-footprint.json")),
+        normalize_json(&to_string_pretty(&schema_for!(ShipmentFootprint)).unwrap())
+    );
+    assert_eq!(
+        normalize_json(&read_schema("pcf-shipment-footprint.json")),
+        normalize_json(
+            &to_string_pretty(&gen_pcf_with_extension::<ShipmentFootprint>(
+                "ShipmentFootprint"
+            ))
+            .unwrap()
+        )
+    );
+
+    assert_eq!(
+        normalize_json(&read_schema("toc.json")),
+        normalize_json(&to_string_pretty(&schema_for!(Toc)).unwrap())
+    );
+    assert_eq!(
+        normalize_json(&read_schema("pcf-toc.json")),
+        normalize_json(&to_string_pretty(&gen_pcf_with_extension::<Toc>("Toc")).unwrap())
+    );
+
+    assert_eq!(
+        normalize_json(&read_schema("hoc.json")),
+        normalize_json(&to_string_pretty(&schema_for!(Hoc)).unwrap())
+    );
+    assert_eq!(
+        normalize_json(&read_schema("pcf-hoc.json")),
+        normalize_json(&to_string_pretty(&gen_pcf_with_extension::<Hoc>("Hoc")).unwrap())
+    );
+
+    assert_eq!(
+        normalize_json(&read_schema("tad.json")),
+        normalize_json(&to_string_pretty(&schema_for!(Tad)).unwrap())
+    );
+    assert_eq!(
+        normalize_json(&read_schema("pcf-tad.json")),
+        normalize_json(&to_string_pretty(&gen_pcf_with_extension::<Tad>("Tad")).unwrap())
+    );
 }
